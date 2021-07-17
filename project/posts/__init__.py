05e5aee2cf6643372c7eb3cc37dd08be5a8b7ff0
BotenAqua <aquapelzacz@gmail.com>
2021-07-17 17:43:24 +0200

    fix static

--- project/posts/__init__.py
+++ project/posts/__init__.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template
 
-posts = Blueprint("posts", __name__, template_folder="templates")
+posts = Blueprint("posts", __name__, template_folder="templates", static_folder="static")
 
 @posts.route("/")
 def global_test():
         page_content = {'title': 'Posts', 'data': 'posts blueprint <3'}
-        return render_template('index.html', **page_content)
+        return render_template('test.html', **page_content)
